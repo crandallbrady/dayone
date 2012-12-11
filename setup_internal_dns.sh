7443a9c4320d6d8a5c024e1b714962da0772d148
Max Beatty <max@isocket.com>
2012-12-11 13:49:24 -0800

    simplify dns setup

--- setup_internal_dns.sh
+++ setup_internal_dns.sh
@@ -2,21 +2,9 @@
 # Setup internal DNS so you can download Xcode from internal server
 dns="192.168.86.200 8.8.8.8 8.8.4.4"
 
-echo "Are you connected via WiFi? (y/n)"
-read wifi
-if [ "$wifi" == "y" ]; then
-    service="Wi-Fi"
-else
-    echo "Are you using Ethernet? (y/n)"
-    read eth
-    if [ "$eth" == "y" ]; then
-        service="Ethernet"
-    else
-        echo "Get online and re-run this script"
-        exit;
-    fi
-fi
-
 # set dns server instead of ...
-# opening system preferences, going into network, selecting device, going to advance, typing it in, oking and applying
-sudo networksetup -setdnsservers $service $dns
+# ... opening system preferences, going into network, selecting device, going to advance, typing it in, oking and applying
+sudo networksetup -setdnsservers Ethernet $dns
+sudo networksetup - setdnsservers Wi-Fi $dns
+
+# TODO: test if internal dns is resolving properly before continuing
