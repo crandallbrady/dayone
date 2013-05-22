c7de4c6d475d19b1d7246bc40a1667af10b05128
Max Beatty <max@isocket.com>
2013-05-22 12:09:37 -0700

    install nvm

--- setup.sh
+++ setup.sh
@@ -45,6 +45,9 @@ rvm reinstall all --force
 # Install RubyGems
 source install_rubygems.sh
 
+# Install Node Version Manager
+source install_nvm.sh
+
 # Set global configs for git
 source git_config.sh
 
